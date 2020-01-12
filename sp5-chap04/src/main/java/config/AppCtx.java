1f43f37fa335b5829738cb7a03d7d92e8dfca33f
yoon <dbstjrwnekd@naver.com>
2020-01-13 03:06:10 +0900

    day1

---
+++ sp5-chap04/src/main/java/config/AppCtx.java
@@ -0,0 +1,69 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import spring.ChangePasswordService;
+import spring.MemberDao;
+import spring.MemberRegisterService;
+import spring.VersionPrinter;
+import spring.MemberListPrinter;
+import spring.MemberPrinter;
+import spring.MemberInfoPrinter;
+
+@Configuration
+public class AppCtx {
+	
+	@Bean
+	public MemberDao memberDao() {
+		return new MemberDao();
+	}
+	
+	@Bean
+	public MemberRegisterService memberRegSvc() {
+		return new MemberRegisterService();
+	}
+	
+	@Bean
+	public ChangePasswordService changePwdSvc() {
+		ChangePasswordService pwdSvc = new ChangePasswordService();
+		return pwdSvc;
+	}
+	
+	/*
+	@Bean
+	public MemberPrinter memberPrinter() {
+		return new MemberPrinter();
+	}
+	*/
+	@Bean
+	@Qualifier("printer")
+	public MemberPrinter memberPrinter1() {
+		return new MemberPrinter();
+	}
+	
+	@Bean
+	public MemberPrinter memberPrinter2() {
+		return new MemberPrinter();
+	}
+	
+	@Bean
+	public MemberListPrinter listPrinter() {
+		return new MemberListPrinter();
+	}
+	
+	@Bean
+	public MemberInfoPrinter infoPrinter() {
+		MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
+		return infoPrinter;
+	}
+	
+	@Bean
+	public VersionPrinter versionPrinter() {
+		VersionPrinter versionPrinter = new VersionPrinter();
+		versionPrinter.setMajorVersion(5);
+		versionPrinter.setMinorVersion(0);
+		return versionPrinter;
+	}
+}
---
+++ sp5-chap04/src/main/java/spring/MemberListPrinter.java
@@ -0,0 +1,31 @@
+package spring;
+
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class MemberListPrinter {
+	
+	private MemberDao memberDao;
+	private MemberPrinter printer;
+	
+	public MemberListPrinter() {
+	}
+	
+	@Autowired
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+	
+	@Autowired
+	@Qualifier("printer")
+	public void setMemberPrinter(MemberPrinter printer) {
+		this.printer = printer;
+	}
+	
+	
+	public void printAll() {
+		Collection<Member> members = memberDao.selectAll();
+		members.forEach(m -> printer.print(m));
+	}
+}
